b1d80b6c7798466774c021b4f7c059168f713d9d
Gary Lo <logap@me.com>
2020-02-09 01:43:39 +0800

    change benchmark case

--- bombardier.sh
+++ bombardier.sh
@@ -2,13 +2,13 @@
 mkdir -p $2;
 
 echo "running warmup...";
-docker run -ti --rm --network=host alpine/bombardier --http1 -o json -p result -c 50 -d 30s -l $1 | jq '.' | tee "$2/warmup.json" > /dev/null;
+docker run -ti --rm --network=host alpine/bombardier --http1 -o json -p result -c 50 -n 50000 -t 60s -l $1 | jq '.' | tee "$2/warmup.json" > /dev/null;
 
 sleep 5;
 
 for value in 500 1000 2000 4000 8000
 do
   echo "running concurrency = $value";
-  docker run -ti --rm --network=host alpine/bombardier --http1 -o json -p result -c $value -d 30s -l $1 | jq '.' | tee "$2/c$value.json" > /dev/null;
+  docker run -ti --rm --network=host alpine/bombardier --http1 -o json -p result -c $value -n 50000 -t 60s -l $1 | jq '.' | tee "$2/c$value.json" > /dev/null;
   sleep 5;
 done
