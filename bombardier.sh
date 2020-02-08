b160b3f8cdc300f6be436e92836732f24a69417d
Gary Lo <logap@me.com>
2020-02-09 01:50:56 +0800

    update benchmark concurrency

--- bombardier.sh
+++ bombardier.sh
@@ -2,11 +2,11 @@
 mkdir -p $2;
 
 echo "running warmup...";
-docker run -ti --rm --network=host alpine/bombardier --http1 -o json -p result -c 50 -n 50000 -t 60s -l $1 | jq '.' | tee "$2/warmup.json" > /dev/null;
+docker run -ti --rm --network=host alpine/bombardier --http1 -o json -p result -c 1000 -n 50000 -t 60s -l $1 | jq '.' | tee "$2/warmup.json" > /dev/null;
 
 sleep 5;
 
-for value in 500 1000 2000 4000 8000
+for value in 800 1600 3200 6400
 do
   echo "running concurrency = $value";
   docker run -ti --rm --network=host alpine/bombardier --http1 -o json -p result -c $value -n 50000 -t 60s -l $1 | jq '.' | tee "$2/c$value.json" > /dev/null;
