5065879d694d95450fa5bc90acca6e50a25b171f
Gary Lo <logap@me.com>
2020-02-09 02:08:02 +0800

    add fixed & reproducible different io time

--- node-backend/index.js
+++ node-backend/index.js
@@ -5,8 +5,13 @@ app.get('/', function (req, res) {
     res.send('ok')
 })
 
-app.get('/io/:io', function (req, res) {
-    setTimeout(() => res.send('ok'), req.params.io)
+const timeouts = [20,40,60,80,100,120,140,160,180,200,400,600,800,1000]
+const len = timeouts.length
+let counter = 0
+
+app.get('io', function (req, res) {
+    setTimeout(() => res.send('ok'), timeouts[counter % len - 1])
+    counter += 1;
 })
 
 console.log('started express...')
