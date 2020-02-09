27aa14fb310a459fb7173c91038673be214d5318
Gary Lo <logap@me.com>
2020-02-09 17:51:24 +0800

    increase ulimit

--- bombardier.sh
+++ bombardier.sh
@@ -4,7 +4,7 @@ mkdir -p $2;
 echo "running warmup...";
 warmup_concurrency=1000
 warmup_result="$2/warmup.json"
-docker run -ti --rm --network=host --ulimit nofile=20000:40000 alpine/bombardier --http2 -o json -p result -c $warmup_concurrency -n 50000 -t 60s -l $1 | jq '.' | tee $warmup_result > /dev/null;
+docker run -ti --rm --network=host --ulimit nofile=65535:65535 alpine/bombardier --http2 -o json -p result -c $warmup_concurrency -n 50000 -t 60s -l $1 | jq '.' | tee $warmup_result > /dev/null;
 echo "concurrency=$warmup_concurrency, timeTakenSeconds=$(cat $warmup_result | jq '.result.timeTakenSeconds'), rps=$(cat $warmup_result | jq '.result.rps.mean')"
 sleep 5;
 
@@ -15,7 +15,7 @@ for value in "${concurrency[@]}"
 do
   json_result="$2/c$value.json"
   echo "running concurrency = $value";
-  docker run -ti --rm --network=host --ulimit nofile=20000:40000 alpine/bombardier --http2 -o json -p result -c $value -n 1000000 -t 60s -l $1 | jq '.' | tee $json_result > /dev/null;
+  docker run -ti --rm --network=host --ulimit nofile=65535:65535 alpine/bombardier --http2 -o json -p result -c $value -n 1000000 -t 60s -l $1 | jq '.' | tee $json_result > /dev/null;
   echo "concurrency=$value, timeTakenSeconds=$(cat $json_result | jq '.result.timeTakenSeconds'), rps=$(cat $json_result | jq '.result.rps.mean')"
   sleep 5;
 done
