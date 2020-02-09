1b3bf9d13747131591e849963c36a632189d97d6
Gary Lo <logap@me.com>
2020-02-09 17:14:42 +0800

    more flexible bench script

--- bombardier.sh
+++ bombardier.sh
@@ -6,7 +6,10 @@ docker run -ti --rm --network=host --ulimit nofile=20000:40000 alpine/bombardier
 
 sleep 5;
 
-for value in 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000
+inputs=( "$@" )
+concurrency=( "${inputs[@]:2}" )
+
+for value in "${concurrency[@]}"
 do
   echo "running concurrency = $value";
   docker run -ti --rm --network=host --ulimit nofile=20000:40000 alpine/bombardier --fasthttp -o json -p result -c $value -n 300000 -t 60s -l $1 | jq '.' | tee "$2/c$value.json" > /dev/null;
