1fb30c4fbf3af14f90f2dab831d15c1ba48639d5
Gary Lo <logap@me.com>
2020-02-09 17:37:10 +0800

    switch to http2

--- bombardier.sh
+++ bombardier.sh
@@ -2,7 +2,7 @@
 mkdir -p $2;
 
 echo "running warmup...";
-docker run -ti --rm --network=host --ulimit nofile=20000:40000 alpine/bombardier --fasthttp -o json -p result -c 1000 -n 50000 -t 60s -l $1 | jq '.' | tee "$2/warmup.json" > /dev/null;
+docker run -ti --rm --network=host --ulimit nofile=20000:40000 alpine/bombardier --http2 -o json -p result -c 1000 -n 50000 -t 60s -l $1 | jq '.' | tee "$2/warmup.json" > /dev/null;
 
 sleep 5;
 
@@ -12,6 +12,6 @@ concurrency=( "${inputs[@]:2}" )
 for value in "${concurrency[@]}"
 do
   echo "running concurrency = $value";
-  docker run -ti --rm --network=host --ulimit nofile=20000:40000 alpine/bombardier --fasthttp -o json -p result -c $value -n 300000 -t 60s -l $1 | jq '.' | tee "$2/c$value.json" > /dev/null;
+  docker run -ti --rm --network=host --ulimit nofile=20000:40000 alpine/bombardier --http2 -o json -p result -c $value -n 300000 -t 60s -l $1 | jq '.' | tee "$2/c$value.json" > /dev/null;
   sleep 5;
 done
